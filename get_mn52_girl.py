451bded10dabe44252b95e32cf32652ee4e5eefe
chrisyekui <316824305@qq.com>
2019-06-27 08:30:21 +0800

    'Reformatcode'

--- get_mn52_girl.py
+++ get_mn52_girl.py
@@ -4,7 +4,6 @@ import time
 import re
 
 
-
 def url_open(url):
     headers = {
         'Referrer': 'https://www.mn52.com/',
@@ -20,84 +19,87 @@ def url_open(url):
     # print(html)
     return html
 
-#主程序
+
+# 主程序
 def get_class(url):
     html = url_open(url)
     get_class = []
-    results = re.findall('<li.*?href=\'\/(.*?)\/\'\s><span>(.*?)</span></a></li>',html,re.S)
+    results = re.findall('<li.*?href=\'\/(.*?)\/\'\s><span>(.*?)</span></a></li>', html, re.S)
     print(results)
     for result in results:
-        if result[1] not in ['头像集','婚纱图片''萌宠图片']:
+        if result[1] not in ['头像集', '婚纱图片''萌宠图片']:
             print(result[1])
-            #已有目录不用重新创建
+            # 已有目录不用重新创建
             if os.path.exists(result[1]):
                 os.chdir(result[1])
             else:
                 os.mkdir(result[1])
                 os.chdir(result[1])
-            class_url = 'https://www.mn52.com/'+result[0]
+            class_url = 'https://www.mn52.com/' + result[0]
             print(class_url)
-            #查找分类页数并每页查找
+            # 查找分类页数并每页查找
             # for i in range(1,int(get_max_pagenum(class_url))+1):
             html1 = url_open(class_url)
-            list_num = re.findall('<div class="itempages">.*?<a class="current">1</a>\r\n.*?<a href=\'(.*?)_2.html\'>2</a>',html1,re.S)
+            list_num = re.findall(
+                '<div class="itempages">.*?<a class="current">1</a>\r\n.*?<a href=\'(.*?)_2.html\'>2</a>', html1, re.S)
             print(list_num)
-            for i in range(1,int(get_max_pagenum(class_url))+1):
-                print(class_url+'/'+list_num[0]+'_'+str(i)+'.html')
-                get_classpage_message(class_url+'/'+list_num[0]+'_'+str(i)+'.html')
-
-
+            for i in range(1, int(get_max_pagenum(class_url)) + 1):
+                print(class_url + '/' + list_num[0] + '_' + str(i) + '.html')
+                get_classpage_message(class_url + '/' + list_num[0] + '_' + str(i) + '.html')
             os.chdir(os.path.pardir)
         else:
             print('不需要这3个分类图片')
 
 
-
 def get_max_pagenum(url):
     try:
         html = url_open(url)
-        max_pagenum = re.findall('下一页</a>\r\n<a href=\'list_.*?_(.*?).html\'>末页</a>.*?<!-- page end -->',html,re.S)
+        max_pagenum = re.findall('下一页</a>\r\n<a href=\'list_.*?_(.*?).html\'>末页</a>.*?<!-- page end -->', html, re.S)
         print(max_pagenum[0])
         return max_pagenum[0]
     except:
-        #获取不到最大页面时返回30
+        # 获取不到最大页面时返回30
         return 30
+
 
 def get_classpage_message(url):
     html = url_open(url)
-    classpage_messages = re.findall('<div class="picbox">.*?<a href="(.*?)" target="_blank"><img src=.*?data-original=.*?alt="(.*?)" width="235" height="350" style="display: inline;"></a>', html, re.S)
-
+    classpage_messages = re.findall(
+        '<div class="picbox">.*?<a href="(.*?)" target="_blank"><img src=.*?data-original=.*?alt="(.*?)" width="235" height="350" style="display: inline;"></a>',
+        html, re.S)
     print(classpage_messages)
     for classpage_message in classpage_messages:
         print(classpage_message)
-        concretePicture_url = "https://www.mn52.com"+classpage_message[0]
+        concretePicture_url = "https://www.mn52.com" + classpage_message[0]
         if os.path.exists(str(classpage_message[1])):
             os.chdir(classpage_message[1])
         else:
             os.mkdir(classpage_message[1])
             os.chdir(classpage_message[1])
-
         Save_Picture(concretePicture_url)
         os.chdir(os.path.pardir)
         # time.sleep(2)
         get_PicturepPage_nums(concretePicture_url)
         # Save_Picture(concretePicture_url)
-
     return concretePicture_url
+
 
 def get_PicturepPage_nums(url):
     html = url_open(url)
-    PicturePage_nums = re.findall('<div class="img-wrap"><a href="javascript:;" hidefocus="true"><img src=.*?title="第(.*?)张" rel=.*?>', html, re.S)
+    PicturePage_nums = re.findall(
+        '<div class="img-wrap"><a href="javascript:;" hidefocus="true"><img src=.*?title="第(.*?)张" rel=.*?>', html,
+        re.S)
     # print(PicturePage_nums)
     print(PicturePage_nums[-1])
-    #返回页码的最大值
+    # 返回页码的最大值
     return PicturePage_nums[-1]
 
-def Save_Picture(url):
 
+def Save_Picture(url):
     html = url_open(url)
-    Picture_vague_urls = re.findall('<div id="content".*?<div id="big-pic">\r\n(.*?)<div class="photo_prev">', html, re.S)
-    Picture_real_urls = re.findall('<img src="(.*?)" />',str(Picture_vague_urls),re.S)
+    Picture_vague_urls = re.findall('<div id="content".*?<div id="big-pic">\r\n(.*?)<div class="photo_prev">', html,
+                                    re.S)
+    Picture_real_urls = re.findall('<img src="(.*?)" />', str(Picture_vague_urls), re.S)
     print(Picture_real_urls)
     for Picture_real_url in Picture_real_urls:
         time.sleep(0.5)
@@ -109,19 +111,19 @@ def Save_Picture(url):
         # browser.get(picture)
         # time.sleep(10)
         # browser.close()
-        with open(filename,'wb') as f:
+        with open(filename, 'wb') as f:
             # img = url_open(picture)
             headers1 = {
-               'Referer':url,
-               'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36',
-                'Accept':'*/*',
-                'Cache-Control':'no-cache',
-                'Host':'image.mn52.com',
-                'accept-encoding':'gzip, deflate',
-                'Connection':'keep-alive'
+                'Referer': url,
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36',
+                'Accept': '*/*',
+                'Cache-Control': 'no-cache',
+                'Host': 'image.mn52.com',
+                'accept-encoding': 'gzip, deflate',
+                'Connection': 'keep-alive'
             }
             try:
-                pic = requests.get(picture,headers = headers1)
+                pic = requests.get(picture, headers=headers1)
                 # print(pic.text)
                 f.write(pic.content)
             except Exception as e:
@@ -129,17 +131,14 @@ def Save_Picture(url):
             continue
 
 
-def download_mn52_mm(folder='img',pages=10000):
+def download_mn52_mm(folder='img', pages=10000):
     if os.path.exists(folder):
         os.chdir(folder)
     else:
         os.mkdir(folder)
         os.chdir(folder)
-
     url = "https://www.mn52.com/"
-
     get_class(url)
-
 
 
 if __name__ == '__main__':
