973bff62dc20f5dfd66457fa35e9e8cc47fb6837
chrisyekui <316824305@qq.com>
2019-06-27 08:23:02 +0800

    '完成mn52.com图片抓取'

--- get_mn52_girl.py
+++ get_mn52_girl.py
@@ -2,12 +2,17 @@ import requests
 import os
 import time
 import re
-from selenium import webdriver
+
 
 
 def url_open(url):
     headers = {
-        'UserAgent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+        'Referrer': 'https://www.mn52.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36',
+        'Accept': '*/*',
+        'Cache-Control': 'no-cache',
+        'accept-encoding': 'gzip, deflate',
+        'Connection': 'keep-alive'
     }
     response = requests.get(url, headers=headers)
     response.encoding = 'utf-8'
@@ -22,26 +27,29 @@ def get_class(url):
     results = re.findall('<li.*?href=\'\/(.*?)\/\'\s><span>(.*?)</span></a></li>',html,re.S)
     print(results)
     for result in results:
-        print(result[1])
-        #已有目录不用重新创建
-        if os.path.exists(result[1]):
-            os.chdir(result[1])
+        if result[1] not in ['头像集','婚纱图片''萌宠图片']:
+            print(result[1])
+            #已有目录不用重新创建
+            if os.path.exists(result[1]):
+                os.chdir(result[1])
+            else:
+                os.mkdir(result[1])
+                os.chdir(result[1])
+            class_url = 'https://www.mn52.com/'+result[0]
+            print(class_url)
+            #查找分类页数并每页查找
+            # for i in range(1,int(get_max_pagenum(class_url))+1):
+            html1 = url_open(class_url)
+            list_num = re.findall('<div class="itempages">.*?<a class="current">1</a>\r\n.*?<a href=\'(.*?)_2.html\'>2</a>',html1,re.S)
+            print(list_num)
+            for i in range(1,int(get_max_pagenum(class_url))+1):
+                print(class_url+'/'+list_num[0]+'_'+str(i)+'.html')
+                get_classpage_message(class_url+'/'+list_num[0]+'_'+str(i)+'.html')
+
+
+            os.chdir(os.path.pardir)
         else:
-            os.mkdir(result[1])
-            os.chdir(result[1])
-        class_url = 'https://www.mn52.com/'+result[0]
-        print(class_url)
-        #打开分类链接
-        # url_open(class_url)
-        get_max_pagenum(class_url)
-        get_classpage_message(class_url)
-
-
-
-
-
-
-        os.chdir(os.path.pardir)
+            print('不需要这3个分类图片')
 
 
 
@@ -58,6 +66,7 @@ def get_max_pagenum(url):
 def get_classpage_message(url):
     html = url_open(url)
     classpage_messages = re.findall('<div class="picbox">.*?<a href="(.*?)" target="_blank"><img src=.*?data-original=.*?alt="(.*?)" width="235" height="350" style="display: inline;"></a>', html, re.S)
+
     print(classpage_messages)
     for classpage_message in classpage_messages:
         print(classpage_message)
@@ -67,10 +76,12 @@ def get_classpage_message(url):
         else:
             os.mkdir(classpage_message[1])
             os.chdir(classpage_message[1])
+
+        Save_Picture(concretePicture_url)
         os.chdir(os.path.pardir)
         # time.sleep(2)
         get_PicturepPage_nums(concretePicture_url)
-        get_Picture_downloadurl(concretePicture_url)
+        # Save_Picture(concretePicture_url)
 
     return concretePicture_url
 
@@ -82,65 +93,41 @@ def get_PicturepPage_nums(url):
     #返回页码的最大值
     return PicturePage_nums[-1]
 
-def get_Picture_downloadurl(url):
+def Save_Picture(url):
+
     html = url_open(url)
     Picture_vague_urls = re.findall('<div id="content".*?<div id="big-pic">\r\n(.*?)<div class="photo_prev">', html, re.S)
-    picture_real_urls = re.findall('<img src="(.*?)" />',str(Picture_vague_urls),re.S)
-    print(picture_real_urls)
-    for picture_real_url in picture_real_urls:
+    Picture_real_urls = re.findall('<img src="(.*?)" />',str(Picture_vague_urls),re.S)
+    print(Picture_real_urls)
+    for Picture_real_url in Picture_real_urls:
+        time.sleep(0.5)
+        picture = 'https:' + Picture_real_url
+        filename = (picture).split('/')[-1]
+        print(filename)
+        # browser = webdriver.Chrome(
+        #     executable_path=r'C:\Users\Administrator\AppData\Local\Google\Chrome\Application\chromedriver.exe')
+        # browser.get(picture)
+        # time.sleep(10)
+        # browser.close()
+        with open(filename,'wb') as f:
+            # img = url_open(picture)
+            headers1 = {
+               'Referer':url,
+               'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36',
+                'Accept':'*/*',
+                'Cache-Control':'no-cache',
+                'Host':'image.mn52.com',
+                'accept-encoding':'gzip, deflate',
+                'Connection':'keep-alive'
+            }
+            try:
+                pic = requests.get(picture,headers = headers1)
+                # print(pic.text)
+                f.write(pic.content)
+            except Exception as e:
+                pass
+            continue
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def get_page(url):
-#     html = url_open(url).decode('utf-8')
-#     print(html)
-#
-#     # a = html.find('current-comment-page') + 23
-#     # b = html.find(']',a)
-#     # return html[a:b]
-#
-# def find_imgs(url):
-#     html = url_open(url).decode('utf-8')
-#     print(html)
-#     img_address = []
-#
-#
-#     # a = html.find('img src=')
-#     #
-#     # while a!= -1:
-#     #     b = html.find('jpg',a,a+255)
-#     #
-#     #     if b!= -1:
-#     #         img_address.append('http:'+html[a+9:b+3])
-#     #     else:
-#     #         b = a + 9
-#     #     a = html.find('img src=',b)
-#
-#     for each in img_address:
-# #       print(each)
-#         return img_address
-#
-#
-# def save_img(folder,img_address):
-#     for each in img_address:
-#         filename = each.split('/')[-1]
-#         with open(filename,'wb') as f:
-#             img = url_open(each)
-#             f.write(img)
 
 def download_mn52_mm(folder='img',pages=10000):
     if os.path.exists(folder):
@@ -150,23 +137,10 @@ def download_mn52_mm(folder='img',pages=10000):
         os.chdir(folder)
 
     url = "https://www.mn52.com/"
-    # url = "https://www.mn52.com/meinvmingxing/26296.html"
 
     get_class(url)
-    # get_PicturepPage_nums(url)
 
 
-# def new_folder(floder=result)
-
-
-        # page_num = int(get_page(url))
-        #
-        # for i in range(pages):
-        #     page_num = i
-        #     page_url = url + 'page-' + str(page_num) + '#comments'
-        #     img_address = find_imgs(page_url)
-        #     save_img(folder,img_address)
-        #     time.sleep(10)
 
 if __name__ == '__main__':
     download_mn52_mm()
