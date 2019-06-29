6f380736a285ed624704422cfb8687425ebb6846
chrisyekui <316824305@qq.com>
2019-06-29 08:46:18 +0800

    '新增反爬useranget'

--- get_mn52_girl.py
+++ get_mn52_girl.py
@@ -2,6 +2,7 @@ import requests
 import os
 import time
 import re
+import random
 
 
 def url_open(url):
@@ -27,7 +28,7 @@ def get_class(url):
     results = re.findall('<li.*?href=\'\/(.*?)\/\'\s><span>(.*?)</span></a></li>', html, re.S)
     print(results)
     for result in results:
-        if result[1] not in ['头像集', '婚纱图片''萌宠图片']:
+        if result[1] not in ['性感美女','头像集', '婚纱图片''萌宠图片']:
             print(result[1])
             # 已有目录不用重新创建
             if os.path.exists(result[1]):
@@ -48,7 +49,7 @@ def get_class(url):
                 get_classpage_message(class_url + '/' + list_num[0] + '_' + str(i) + '.html')
             os.chdir(os.path.pardir)
         else:
-            print('不需要这3个分类图片')
+            print('不需要这几个分类图片')
 
 
 def get_max_pagenum(url):
@@ -71,11 +72,12 @@ def get_classpage_message(url):
     for classpage_message in classpage_messages:
         print(classpage_message)
         concretePicture_url = "https://www.mn52.com" + classpage_message[0]
-        #过滤特殊字符，只保留中英文和数字
+        #过滤特殊字符，只保留中英文和数字，否则无法创建文件夹
         # str = re.sub('[a-zA-Z0-9’!"#$%&\'()*+,-./:;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~\s]+', "", classpage_message[1])
         str = re.sub('[’!"#$%&\'()*+,-./:;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~\s]+', '', classpage_message[1])
         if os.path.exists(str):
             try:
+                # 此方法适合碰到一个异常解决一个异常
                 # os.chdir(classpage_message[1].replace(':','').replace('|',''))
                 os.chdir(str)
             except NotADirectoryError:
@@ -117,6 +119,8 @@ def Save_Picture(url):
                                     re.S)
     Picture_real_urls = re.findall('<img src="(.*?)" />', str(Picture_vague_urls), re.S)
     print(Picture_real_urls)
+    # 打印当前时间
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
     for Picture_real_url in Picture_real_urls:
         #做个间隔，发现下得太慢了，放弃
         # time.sleep(0.5)
@@ -125,6 +129,8 @@ def Save_Picture(url):
             picture = 'https:' + Picture_real_url
         elif Picture_real_url[:3] == '/im':
             picture = 'https://image.mn52.com' + Picture_real_url
+        else:
+            picture = Picture_real_url
         filename = (picture).split('/')[-1]
         print(filename)
         #尝试使用打开浏览器方式获取，太慢，放弃，UI自动化操作还是不靠谱
@@ -136,9 +142,27 @@ def Save_Picture(url):
         with open(filename, 'wb') as f:
             # img = url_open(picture)
             #尤其注意Referer
+            UserAgentlist = ['Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36',
+                             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+                             'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+                             'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+                             'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+                             'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+                             'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+                             'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+                             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+                             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)',
+                             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)',
+                             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+                             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)',
+                             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+                             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
+                             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)']
+
+            USER_AGENT = random.choice(UserAgentlist)
             headers1 = {
                 'Referer': url,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36',
+                'User-Agent': USER_AGENT,
                 'Accept': '*/*',
                 'Cache-Control': 'no-cache',
                 'Host': 'image.mn52.com',
@@ -146,9 +170,8 @@ def Save_Picture(url):
                 'Connection': 'keep-alive'
             }
             try:
-                #设置5秒超时限制
+                #设置10秒超时限制
                 pic = requests.get(picture, headers=headers1, timeout = 10)
-                # print(pic.text)
                 f.write(pic.content)
             except Exception as e:
                 pass
